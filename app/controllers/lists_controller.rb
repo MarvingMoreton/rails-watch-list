8893b9c20ff5e1ef7420f3703e3c8ae7b1886352
Marving Moreton <moreton.marving@gmail.com>
2022-08-11 14:18:59 -0400

    ListsController

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,30 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    # if creation is a success
+    if @list.save
+      redirect_to list_path(@list), notice: 'List was successfully created.'
+      # if creation is a failure
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @list = List.find((params[:id]))
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
