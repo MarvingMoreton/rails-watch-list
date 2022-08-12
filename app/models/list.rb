33bde922d046165f08bf9625520e6388a61cbecb
Marving Moreton <moreton.marving@gmail.com>
2022-08-12 14:09:29 -0400

    s

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
-  has_one_attached :photo
+  has_many_attached :photos
   # ** order matters: put relation with bookmarks before association through
   has_many :movies, through: :bookmarks
 
