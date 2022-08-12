d95f05eaffdf0812838a3d75f4eed6e36a48721c
Marving Moreton <moreton.marving@gmail.com>
2022-08-12 16:00:44 -0400

    x

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ movies['results'].each do |movie|
   Movie.create!(
     title: movie['title'],
     overview: movie['overview'],
-    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    # poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
     rating: movie['vote_average']
   )
 end
